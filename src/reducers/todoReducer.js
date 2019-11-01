5375da6e53b989fcee317f759499951785b5cd92
cladams0203 <cladams0203@gmail.com>
2019-10-31 21:36:29 -0400

    created basic reducer and initial state

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    item: '',
+    completed: false,
+    id: Math.random()
+}
+
+export function todoReducer(state, action) {
+    switch(action.type) {
+        default: 
+            return state
+    }
+}
